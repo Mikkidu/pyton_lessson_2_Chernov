73e53c461bc56c7358d609af4d015ddef0c5cabf
Алёша <mikkidu1@gmail.com>
2022-02-13 12:18:39 +0300

    task 5  done

---
+++ 5_quest.py
@@ -0,0 +1,9 @@
+'''
+Задача 5
+Вывести цифры числа на каждой строчке.
+'''
+
+integer_number = input("Пожалуйста, введите число: ")
+
+for i in range(len(integer_number)):
+    print(integer_number[i])
